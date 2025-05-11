cd6ab346026b66c57a1f6a30275b525ba8d62b1c
Raunaq Gupta <notify@raunaqgupta.com>
2025-05-10 17:16:59 -0700

    Upgrade

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,10 @@
+
+export default new Map([
+["src/content/projects/planetscale-docs-refresh.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fplanetscale-docs-refresh.mdx&astroContentModuleFlag=true")],
+["src/content/projects/planetscale-v1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fplanetscale-v1.mdx&astroContentModuleFlag=true")],
+["src/content/projects/planetscale-v2-vision-login.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fplanetscale-v2-vision-login.mdx&astroContentModuleFlag=true")],
+["src/content/recipes/asparagus-cottage-cheese-sandwich.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecipes%2Fasparagus-cottage-cheese-sandwich.mdx&astroContentModuleFlag=true")],
+["src/content/recipes/chai.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecipes%2Fchai.mdx&astroContentModuleFlag=true")],
+["src/content/recipes/potato-chip-sandwich.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecipes%2Fpotato-chip-sandwich.mdx&astroContentModuleFlag=true")],
+["src/content/recipes/mango-lassi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecipes%2Fmango-lassi.mdx&astroContentModuleFlag=true")]]);
+		

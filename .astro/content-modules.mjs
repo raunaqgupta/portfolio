993f6cfbb609dd87e093524e3e116635b0f2e2dd
Raunaq Gupta <notify@raunaqgupta.com>
2025-08-16 12:23:55 -0700

    Upgrade astro and remove stuff from ps login vision project

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,14 +1,14 @@
 
 export default new Map([
-["src/content/projects/planetscale-v1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fplanetscale-v1.mdx&astroContentModuleFlag=true")],
-["src/content/projects/objects.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fobjects.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/planetscale-docs-refresh.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fplanetscale-docs-refresh.mdx&astroContentModuleFlag=true")],
-["src/content/projects/planetscale-v2-vision-login.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fplanetscale-v2-vision-login.mdx&astroContentModuleFlag=true")],
-["src/content/recipes/california-breakfast-bowl.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecipes%2Fcalifornia-breakfast-bowl.mdx&astroContentModuleFlag=true")],
+["src/content/projects/objects.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fobjects.mdx&astroContentModuleFlag=true")],
+["src/content/projects/planetscale-v1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fplanetscale-v1.mdx&astroContentModuleFlag=true")],
 ["src/content/recipes/asparagus-cottage-cheese-sandwich.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecipes%2Fasparagus-cottage-cheese-sandwich.mdx&astroContentModuleFlag=true")],
-["src/content/recipes/chai.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecipes%2Fchai.mdx&astroContentModuleFlag=true")],
+["src/content/projects/planetscale-v2-vision-login.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fplanetscale-v2-vision-login.mdx&astroContentModuleFlag=true")],
 ["src/content/recipes/feta-sausage-tomato-mustard-sandwich.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecipes%2Ffeta-sausage-tomato-mustard-sandwich.mdx&astroContentModuleFlag=true")],
 ["src/content/recipes/mango-lassi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecipes%2Fmango-lassi.mdx&astroContentModuleFlag=true")],
-["src/content/recipes/potato-chip-sandwich.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecipes%2Fpotato-chip-sandwich.mdx&astroContentModuleFlag=true")],
-["src/content/recipes/yogurt-dip.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecipes%2Fyogurt-dip.mdx&astroContentModuleFlag=true")]]);
+["src/content/recipes/california-breakfast-bowl.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecipes%2Fcalifornia-breakfast-bowl.mdx&astroContentModuleFlag=true")],
+["src/content/recipes/yogurt-dip.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecipes%2Fyogurt-dip.mdx&astroContentModuleFlag=true")],
+["src/content/recipes/chai.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecipes%2Fchai.mdx&astroContentModuleFlag=true")],
+["src/content/recipes/potato-chip-sandwich.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecipes%2Fpotato-chip-sandwich.mdx&astroContentModuleFlag=true")]]);
 		

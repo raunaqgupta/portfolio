26ef5b3dd224bf0e62ff7ab5ebea0138a35e0e8b
Raunaq Gupta <account@raunaqgupta.com>
2025-11-27 12:41:42 -0800

    icons

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,18 +1,18 @@
 
 export default new Map([
-["src/content/casestudies/objects.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcasestudies%2Fobjects.mdx&astroContentModuleFlag=true")],
-["src/content/casestudies/appsec-for-appdynamics.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcasestudies%2Fappsec-for-appdynamics.mdx&astroContentModuleFlag=true")],
 ["src/content/casestudies/appopt-for-splunk-o11y-cloud.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcasestudies%2Fappopt-for-splunk-o11y-cloud.mdx&astroContentModuleFlag=true")],
 ["src/content/casestudies/dbmon-for-splunk-o11y-cloud.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcasestudies%2Fdbmon-for-splunk-o11y-cloud.mdx&astroContentModuleFlag=true")],
-["src/content/casestudies/planetscale-v1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcasestudies%2Fplanetscale-v1.mdx&astroContentModuleFlag=true")],
-["src/content/casestudies/planetscale-v2-vision-login.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcasestudies%2Fplanetscale-v2-vision-login.mdx&astroContentModuleFlag=true")],
-["src/content/casestudies/planetscale-docs-refresh.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcasestudies%2Fplanetscale-docs-refresh.mdx&astroContentModuleFlag=true")],
+["src/content/casestudies/appsec-for-appdynamics.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcasestudies%2Fappsec-for-appdynamics.mdx&astroContentModuleFlag=true")],
 ["src/content/casestudies/planetscale.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcasestudies%2Fplanetscale.mdx&astroContentModuleFlag=true")],
-["src/content/blog/california-breakfast-bowl.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fcalifornia-breakfast-bowl.mdx&astroContentModuleFlag=true")],
+["src/content/casestudies/planetscale-v2-vision-login.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcasestudies%2Fplanetscale-v2-vision-login.mdx&astroContentModuleFlag=true")],
+["src/content/casestudies/planetscale-v1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcasestudies%2Fplanetscale-v1.mdx&astroContentModuleFlag=true")],
+["src/content/casestudies/objects.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcasestudies%2Fobjects.mdx&astroContentModuleFlag=true")],
+["src/content/casestudies/planetscale-docs-refresh.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcasestudies%2Fplanetscale-docs-refresh.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/asparagus-cottage-cheese-sandwich.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fasparagus-cottage-cheese-sandwich.mdx&astroContentModuleFlag=true")],
+["src/content/blog/california-breakfast-bowl.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fcalifornia-breakfast-bowl.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/chai.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fchai.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/feta-sausage-tomato-mustard-sandwich.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ffeta-sausage-tomato-mustard-sandwich.mdx&astroContentModuleFlag=true")],
-["src/content/blog/potato-chip-sandwich.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fpotato-chip-sandwich.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/mango-lassi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmango-lassi.mdx&astroContentModuleFlag=true")],
+["src/content/blog/potato-chip-sandwich.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fpotato-chip-sandwich.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/yogurt-dip.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fyogurt-dip.mdx&astroContentModuleFlag=true")]]);
 		

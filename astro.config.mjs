2d83236af2db7f6c75b9c03f9c73764cc45ab94b
Raunaq Gupta <account@raunaqgupta.com>
2025-12-22 23:37:10 -0800

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://raunaqgupta.com",
   prefetch: true,
   integrations: [mdx(), sitemap()],
   image: {

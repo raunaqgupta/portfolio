065aa01a62737a967c34b71dc5f8bf2fb9b8ec1f
Raunaq Gupta <account@raunaqgupta.com>
2025-12-22 23:32:29 -0800

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   image: {
     layout: "constrained"
   },

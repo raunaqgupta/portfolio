e13a23bad1b5176fdeb5975ab79fd442c53ead41
Raunaq Gupta <account@raunaqgupta.com>
2026-01-20 07:42:29 -0800

    update

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -28,5 +28,6 @@ export default new Map([
 ["src/content/articles/docs.planetscale.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fdocs.planetscale.mdx&astroContentModuleFlag=true")],
 ["src/content/articles/docs-planetscale.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fdocs-planetscale.mdx&astroContentModuleFlag=true")],
 ["src/content/articles/flux-planetscale.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fflux-planetscale.mdx&astroContentModuleFlag=true")],
-["src/content/articles/login-concept-planetscale.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Flogin-concept-planetscale.mdx&astroContentModuleFlag=true")]]);
+["src/content/articles/login-concept-planetscale.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Flogin-concept-planetscale.mdx&astroContentModuleFlag=true")],
+["src/content/articles/appopt.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fappopt.mdx&astroContentModuleFlag=true")]]);
 		
